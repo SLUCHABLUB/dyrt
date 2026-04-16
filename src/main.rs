35fc09b21a0f23c5f1a51d960730978aebe1dc9f
Nelli Skogman <nelli.skogman@gmail.com>
2026-04-16 20:50:33 +0200

    Port to rat-salsa

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,41 @@ mod processing;
 use crate::colours::COLOURS;
 use crate::expense::Expense;
 use crate::plot::Plot;
-use anyhow::Context;
+use anyhow::Context as _;
 use clap::Parser;
 use enum_iterator::all;
 use enum_iterator::next_cycle;
 use enum_iterator::previous_cycle;
-use ratatui::DefaultTerminal;
+use rat_salsa::Control;
+use rat_salsa::RunConfig;
+use rat_salsa::SalsaAppContext;
+use rat_salsa::mock;
+use rat_salsa::poll::PollCrossterm;
+use rat_salsa::run_tui;
+use rat_widget::event::ct_event;
+use ratatui::buffer::Buffer;
 use ratatui::crossterm::event::Event;
-use ratatui::crossterm::event::KeyCode;
-use ratatui::crossterm::event::KeyModifiers;
-use ratatui::crossterm::event::poll;
-use ratatui::crossterm::event::read;
 use ratatui::layout::Constraint;
 use ratatui::layout::Layout;
-use ratatui::run;
+use ratatui::layout::Rect;
 use ratatui::widgets::Block;
+use ratatui::widgets::StatefulWidget;
 use ratatui::widgets::Tabs;
+use ratatui::widgets::Widget as _;
 use ratatui_image::FilterType;
 use ratatui_image::Resize;
 use ratatui_image::StatefulImage;
 use ratatui_image::picker::Picker;
 use std::path::Path;
 use std::sync::LazyLock;
-use std::time::Duration;
+
+type Context = SalsaAppContext<Event, anyhow::Error>;
+
+struct State {
+    plot_type: Plot,
+    expenses: &'static [Expense],
+    picker: Picker,
+}
 
 #[derive(Parser)]
 struct Arguments {
@@ -46,83 +58,75 @@ fn main() -> anyhow::Result<()> {
         .deserialize::<Expense>()
         .collect::<Result<_, _>>()?;
 
-    let mut plot_type = Plot::default();
+    let expenses = Vec::leak(expenses);
 
-    let mut should_redraw = true;
+    let mut state = State {
+        plot_type: Plot::default(),
+        expenses,
+        picker,
+    };
 
-    run(|terminal| {
-        loop {
-            if should_redraw {
-                should_redraw = false;
-                redraw(terminal, &expenses, &picker, plot_type)?;
-            }
+    run_tui(
+        mock::init,
+        render,
+        event,
+        error,
+        &mut Context::default(),
+        &mut state,
+        RunConfig::default()?.poll(PollCrossterm),
+    )
+}
 
-            while poll(Duration::ZERO)? {
-                let event = read()?;
+fn render(
+    area: Rect,
+    buffer: &mut Buffer,
+    state: &mut State,
+    _: &mut Context,
+) -> anyhow::Result<()> {
+    let tabs = Tabs::new(all::<Plot>().map(|plot| plot.to_string()))
+        .select(all::<Plot>().position(|plot| plot == state.plot_type));
 
-                match event {
-                    Event::Key(event) => {
-                        if !event.is_press() {
-                            continue;
-                        }
+    let block = Block::bordered().title("Expenses Over Time");
 
-                        match (event.modifiers, event.code) {
-                            (KeyModifiers::CONTROL, KeyCode::Char('q')) => return anyhow::Ok(()),
-                            (KeyModifiers::NONE, KeyCode::Tab | KeyCode::Right) => {
-                                plot_type = next_cycle(&plot_type);
-                                should_redraw = true;
-                            }
-                            (KeyModifiers::SHIFT, KeyCode::Tab)
-                            | (KeyModifiers::NONE, KeyCode::BackTab | KeyCode::Left) => {
-                                plot_type = previous_cycle(&plot_type);
-                                should_redraw = true;
-                            }
-                            _ => (),
-                        }
-                    }
-                    Event::Mouse(_event) => {
-                        // TODO: Clicking buttons.
-                        // TODO: Zooming the graph.
-                        // TODO: Panning the graph.
-                    }
-                    Event::Resize(_, _) => should_redraw = true,
-                    _ => (),
-                }
-            }
-        }
-    })?;
+    let image = state.plot_type.make_image(state.expenses)?;
+    let mut image_state = state.picker.new_resize_protocol(image);
+
+    let [tab_area, content_area] =
+        Layout::vertical([Constraint::Max(3), Constraint::Fill(1)]).areas(area);
+    let image_area = block.inner(content_area);
+
+    tabs.render(tab_area, buffer);
+    block.render(content_area, buffer);
+    StatefulImage::new()
+        .resize(Resize::Scale(Some(FilterType::Gaussian)))
+        .render(image_area, buffer, &mut image_state);
 
     Ok(())
 }
 
-fn redraw(
-    terminal: &mut DefaultTerminal,
-    expenses: &[Expense],
-    picker: &Picker,
-    plot_type: Plot,
-) -> anyhow::Result<()> {
-    let tabs = Tabs::new(all::<Plot>().map(|plot| plot.to_string()))
-        .select(all::<Plot>().position(|plot| plot == plot_type));
+fn event(event: &Event, state: &mut State, _: &mut Context) -> anyhow::Result<Control<Event>> {
+    match event {
+        ct_event!(key press CONTROL-'q') => Ok(Control::Quit),
+        ct_event!(keycode press Tab) | ct_event!(keycode press Right) => {
+            state.plot_type = next_cycle(&state.plot_type);
+            Ok(Control::Changed)
+        }
+        ct_event!(keycode press BackTab)
+        | ct_event!(keycode press SHIFT-Tab)
+        | ct_event!(keycode press Left) => {
+            state.plot_type = previous_cycle(&state.plot_type);
+            Ok(Control::Changed)
+        }
+        ct_event!(mouse any for _event) => {
+            // TODO: Clicking buttons.
+            // TODO: Zooming the graph.
+            // TODO: Panning the graph.
+            Ok(Control::Continue)
+        }
+        _ => Ok(Control::Continue),
+    }
+}
 
-    let block = Block::bordered().title("Expenses Over Time");
-
-    let image = plot_type.make_image(expenses)?;
-    let mut image_state = picker.new_resize_protocol(image);
-
-    terminal.draw(|frame| {
-        let area = frame.area();
-        let [tab_area, content_area] =
-            Layout::vertical([Constraint::Max(3), Constraint::Fill(1)]).areas(area);
-        let image_area = block.inner(content_area);
-
-        frame.render_widget(tabs, tab_area);
-        frame.render_widget(block, content_area);
-        frame.render_stateful_widget(
-            StatefulImage::new().resize(Resize::Scale(Some(FilterType::Gaussian))),
-            image_area,
-            &mut image_state,
-        );
-    })?;
-
-    Ok(())
+fn error(error: anyhow::Error, _: &mut State, _: &mut Context) -> anyhow::Result<Control<Event>> {
+    Err(error)
 }
